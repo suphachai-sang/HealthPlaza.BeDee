d4a5da4f1aefa1687d3be67091dde755cb1af11a
Suphachai Sang <31473954+suphachai-sang@users.noreply.github.com>
2024-04-06 21:25:58 +0700

    2

--- NodeJs-BE/server.js
+++ NodeJs-BE/server.js
@@ -1,9 +1,37 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello! Node.js");
+app.use(bodyParser.json());
+let todos = [];
+
+// Get all todos
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+// Create a new todo
+app.post('/todos', (req, res) => {
+  const { title, description } = req.body;
+  const newTodo = { title, description };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+// Update a todo
+app.put('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, description } = req.body;
+  todos[id] = { title, description };
+  res.json(todos[id]);
+});
+
+// Delete a todo
+app.delete('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const deletedTodo = todos.splice(id, 1);
+  res.json(deletedTodo);
 });
 
 app.listen(port, () => {
--- README.md
+++ README.md
@@ -3,5 +3,31 @@
  
 # npm init -y
 # npm install express --save
+
+
+
+# run 
 # node server.js
 
+
+## Get todos
+curl --location 'http://localhost:3000/todos'
+
+## Post todos
+curl --location 'http://localhost:3000/todos' \
+--header 'Content-Type: application/json' \
+--data '{
+    "title": "Backend Test",
+    "description": "This is Backend Test"
+}'
+
+## Update todos
+curl --location --request PUT 'http://localhost:3000/todos/0' \
+--header 'Content-Type: application/json' \
+--data '{
+    "title": "Backend Test Updated",
+    "description": "This is Backend Test : Updated"
+}'
+
+## Delete todos
+curl --location --request DELETE 'http://localhost:3000/todos/0'
