92919621fbfc58d2c049b027f7c9ffdc9c4ef0fa
Suphachai Sang <31473954+suphachai-sang@users.noreply.github.com>
2024-04-07 13:27:56 +0700

    update common prefix : change to use sorting

--- NodeJs-BE/server.js
+++ NodeJs-BE/server.js
@@ -37,23 +37,48 @@ app.delete('/todos/:id', (req, res) => {
 // Technical Test : Find longest common prefix
 app.post('/findcommon', (req, res) => {
   const { strs } = req.body;
- 
-  function longestCommonPrefix(strs) {
-    if (strs.length === 0) return ""; 
 
-    for (let i = 0; i < strs[0].length; i++) {
-      for (let j = 1; j < strs.length; j++) {
-        if (i >= strs[j].length || strs[j][i] !== strs[0][i]) {
-        
-          return strs[0].substring(0, i);
-        }
+  function longestCommonPrefix(strs) {
+    const size = strs.length;
+
+    //Constraints * 1 <= strs.length <= 200
+    if (size < 1 || size > 200) return "Number of strings must be between 1 and 200.";
+
+    //Constraints * 0 <= strs[i].length <= 200
+    for (let i = 0; i < size; i++) {
+      if (strs[i].length <= 0 || strs[i].length > 200) {
+        return `Length of string at index ${i} must be between 0 and 200.`;
+      }
+
+      //Constraints * strs[i] consists of only lower-case English letters.
+      if (strs[i].toLowerCase() !== strs[i]) {
+        return `String at index ${i} must consist of only lowercase characters.`;
+      }
+    }
+    
+    for (let i = 0; i < size; i++) {
+      if (strs[i].length <= 0 || strs[i].length > 200) {
+        return `Length of string at index ${i} must be between 0 and 200.`;
       }
     }
 
-    return strs[0];
+    if (size === 1) return strs[0];
+
+    strs.sort();
+    // if need to sort and tolowercase 
+    //strs.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+    //console.log(strs);
+
+    const end = Math.min(strs[0].length, strs[size - 1].length);
+
+    let i = 0;
+    while (i < end && strs[0][i].toLowerCase() === strs[size - 1][i].toLowerCase()) 
+      i++;
+    
+    return strs[0].substring(0, i);
   }
 
-  const result = longestCommonPrefix(strs);
+  const result = longestCommonPrefix(strs).toLowerCase();
   res.status(200).json({ commonPrefix: result });
 });
 
